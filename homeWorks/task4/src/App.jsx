82c9e57edba2731c58fec905abbc0997168bf167
zhekazp <zhekazp21@gmail.com>
2024-05-31 11:37:05 +0300

    editing

--- homeWorks/task4/src/App.jsx
+++ homeWorks/task4/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import { Header } from './layout/Header';
 import { Main } from './layout/Main';
 import { Footer } from './layout/Footer';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
